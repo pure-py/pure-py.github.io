1f489240f7eed94870721195ef37ef0b99dcd8d6
Jacob Pake <github@pake.dev>
2026-06-29 10:54:27 +0100

    use absolute paths in nav

--- src/lib/config/nav.ts
+++ src/lib/config/nav.ts
@@ -1,6 +1,6 @@
 export const headerLinks = [
-  { url: "#overview", title: "Overview" },
-  { url: "#features", title: "Features" },
+  { url: "/#overview", title: "Overview" },
+  { url: "/#features", title: "Features" },
   {
     url: "https://github.com/pure-py/pure-py-spec/releases/latest",
     title: "Spec",
